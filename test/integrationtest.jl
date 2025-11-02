29e549719b1865eaf0b82b7d18a8a6927b692e9c
ZoltanRacz <zoltanracz.49@gmail.com>
2025-11-02 14:10:07 +0100

    try testing

--- test/integrationtest.jl
+++ test/integrationtest.jl
@@ -1,9 +1,18 @@
-using .HMMDiscretization
+# useful lines for testing manually, while developing. Install TestEnv in your main environment. When running the first time, activate and instantiate the test environment before restarting Julia and using TestEnv. For more info check: https://github.com/JuliaTesting/TestEnv.jl/blob/main/README.md
+#using TestEnv
+#TestEnv.activate()
+using HMMDiscretization
 using Test
 
 include("examples/VAR.jl")
 
 cp = VAR(B = [1.0 0.0; 0.0 1.0], Σ = [1.0 0.0; 0.0 1.0])
 
-np = HMMNumericalParameters(T = 10^7, m = 11, T0 = 100)
+np = HMMNumericalParameters(T = 10^4, m = 11, T0 = 100)
 
+sim = simulate_continuous(cp, np)
+
+@test cp isa Array{Float64}
+
+#using Plots
+#plot(sim')
