b742c485050765c41f118aa615c131f8b00c0721
ZoltanRacz <zoltanracz.49@gmail.com>
2025-11-06 01:00:36 +0100

    fix test

--- test/integrationtest.jl
+++ test/integrationtest.jl
@@ -1,6 +1,3 @@
-# useful lines for testing manually, while developing. Install TestEnv in your main environment. When running the first time, activate and instantiate the test environment before restarting Julia and using TestEnv. For more info check: https://github.com/JuliaTesting/TestEnv.jl/blob/main/README.md
-#using TestEnv
-#TestEnv.activate()
 using HMMDiscretization
 using Test
 
@@ -10,9 +7,10 @@ include("examples/VAR.jl")
 
 cp = VAR(B = [0.7 0.2; 0.2 0.7], Σ = [1.0 0.0; 0.0 1.0])
 
-np = HMMNumericalParameters(T = 4, N = 10000, m = 15, T0 = 100)
+np = HMMNumericalParameters(T = 4, N = 10000, m = 5, T0 = 100)
 
 sim = simulate_continuous(cp, np)
+@test sim isa Array{Float64, 3}
 
 d0 = HMMDiscretization.default_dp(np,sim)
 
@@ -20,18 +18,12 @@ d0 = HMMDiscretization.default_dp(np,sim)
 @test maximum(abs.(sum(d0.Π, dims = 2) .- ones(np.m,1)))<10^-5
 
 d = discretization(np,sim)
+@test d isa HMMDiscretization.HMMDiscretizedParameters{Float64}
 
 simd = HMMDiscretization.simulate_discrete(d, np)
 
-simd0 = HMMDiscretization.simulate_discrete(d0, np)
-
 mc = HMMDiscretization.moment_comparison(sim,simd)
-
-mc0 = HMMDiscretization.moment_comparison(sim,simd0)
-
-
-@test sim isa Array{Float64, 3}
-@test d isa HMMDiscretization.HMMDiscretizedParameters{Float64}
+@test abs(mc[1][2,2] - mc[1][2,3]) < 0.01
 
 #using Plots
 #scatter(d0.μ[:,1], d0.μ[:,2])
